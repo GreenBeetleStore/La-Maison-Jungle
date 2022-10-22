09516aa1794d30280512cd49ed109d154aa05187
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-10-22 18:02:51 +0200

    attribut style

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import "../styles/Banner.css";
+
+function Banner() {
+  return (
+    <div className="lmj-banner">
+      <h1>La Maison Jungle 🌴</h1>
+    </div>
+  );
+}
+
+export default Banner;
