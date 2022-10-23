14c5fe349c60a5b706c4dfd5da97bef520032bfe
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-10-23 09:54:52 +0200

    P3C2

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "../styles/Cart.css";
 
-function Cart() {
-  const monsteraPrice = 8;
-  const [cart, updateCart] = useState(0);
-  const [isOpen, setIsOpen] = useState(true);
+function Cart({ cart, updateCart }) {
+    const [isOpen, setIsOpen] = useState(true);
+    const total = cart.reduce(
+      (acc, plantType) => acc + plantType.amount * plantType.price,
+      0
+    )
 
   return isOpen ? (
     <div className="lmj-cart">
@@ -15,9 +17,13 @@ function Cart() {
         Fermer
       </button>
       <h2>Panier</h2>
-      <div>Monstera : {monsteraPrice}€</div>
-      <button onClick={() => updateCart(cart + 1)}>Ajouter</button>
-      <h3>Total : {monsteraPrice * cart}€</h3>
+      {cart.map(({ name, price, amount }, index) => (
+            <div key={`${name}-${index}`}>
+                  {name} {price}€ x {amount}
+            </div>
+      ))}
+      
+      <h3>Total : {total}€</h3>
       <button onClick={() => updateCart(0)}>Vider le panier</button>
     </div>
   ) : (
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,15 +2,30 @@ import { plantList } from "../datas/plantList";
 import PlantItem from "./PlantItem";
 import "../styles/ShoppingList.css";
 
-function ShoppingList() {
+function ShoppingList({ cart, updateCart}) {
   const categories = plantList.reduce(
     (acc, plant) =>
       acc.includes(plant.category) ? acc : acc.concat(plant.category),
     []
   );
 
+function addToCart(name, price) {
+      const currentPlantAdded = cart.find((plant) => plant.name === name)
+      if (currentPlantAdded) {
+            const cartFilteredCurrentPlant = cart.filter(
+                  (plant) => plant.name !== name
+            )
+            updateCart([
+                  ...cartFilteredCurrentPlant,
+                  { name, price, amount: currentPlantAdded.amount + 1}
+            ])
+      } else {
+            updateCart([...cart, { name, price, amount: 1 }])
+      }
+}
+
   return (
-    <div>
+    <div className="lmj-shopping-list">
       <ul>
         {categories.map((cat) => (
           <li key={cat}>{cat}</li>
@@ -18,13 +33,10 @@ function ShoppingList() {
       </ul>
       <ul className="lmj-plant-list">
         {plantList.map(({ id, cover, name, water, light }) => (
-          <PlantItem
-            id={id}
-            cover={cover}
-            name={name}
-            water={water}
-            light={light}
-          />
+          <div key={id}>
+            <PlantItem cover={cover} name={name} water={water} light={light} />
+            <button onClick={() => updateCart(cart + 1)}>Ajouter</button>
+            </div>
         ))}
       </ul>
     </div>
